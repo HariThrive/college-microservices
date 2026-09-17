4883561ad04df7f43628f204cff36aaf0c66b4da
hariram <hramgain@gmail.com>
2026-09-17 22:38:13 +0530

    removed_@entity_in_resultVo

--- result/src/main/java/com/college/result/vo/ScoreVO.java
+++ result/src/main/java/com/college/result/vo/ScoreVO.java
@@ -1,11 +1,11 @@
 package com.college.result.vo;
 
-import jakarta.persistence.Entity;
 import lombok.Data;
 
-@Entity
 @Data
 public class ScoreVO {
+    
+    private Long id;
     private Long studentId;
     private Long courseId;
     private Integer marks;
